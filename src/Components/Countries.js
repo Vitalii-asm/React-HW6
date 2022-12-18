882c5928b41396034e9be2b2766e8e9d1a7eb3cc
Vitalii <vitaliiasmolov@gmail.com>
2022-12-18 23:04:46 +0200

    JSX finish

---
+++ src/Components/Countries.js
@@ -0,0 +1,7 @@
+export default  [
+	'EU',
+	'USA',
+	'Great Britain',
+	'Ukraine',
+	'Other'
+];
---
+++ src/Components/Register.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import countries from './Countries.js';
+
+export default function Register() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [country, setCountry] = useState("")
+	const [accepted, setAccepted] = useState("false");
+
+	const handleSubmit = e => { 
+		e.preventDefault();
+		console.log(`
+			Email: ${email};
+			Password: ${password};
+			Country: ${country};
+			Accepted: ${accepted};
+		`)
+	}
+
+
+
+  return (
+	  <form onSubmit={handleSubmit}>
+		  <h1>Create account</h1>
+		  <label>Email:</label>
+		  <input
+			  name='email'
+			  type='email'
+			  value={email}
+			  onChange={e => setEmail(e.target.value)} 
+			  required
+		  />
+
+		  <label>Password:</label>
+		  <input
+			  name='password'
+			  type='password'
+			  value={password}
+			  onChange={e => setPassword(e.target.value)}
+			  required
+		  />
+
+		  <label>Country</label>
+		  <select
+			  name='country'
+			  type='country'
+			  value={country}
+			  onChange={e => setCountry(e.target.value)}
+			  required>
+			  <option key=""></option>
+			  {
+				  countries.map(country => (
+					  <option key={country}> {country}</option>
+					))
+			  }
+			  
+			  </select>
+
+		  <label>
+		  <input
+			  name='accepted'
+			  type='checkbox'
+			  value={accepted}
+			  required
+			  />
+			  Accepted
+		  </label>
+		  <button>submit</button>
+		  
+	 </form>
+  )
+}
+
+
+
+
